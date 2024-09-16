83e4feb36a9bcfcce3cf2d1af8055a62fb090fab
Andrei <andrei.baditoiu@gmail.com>
2024-09-16 14:00:30 +0300

    Modified server.py, renamed(moved) server.js, style.css. Added convertor.py, favicon.ico.

--- server.py
+++ server.py
@@ -1,22 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 print(__name__)
 
 
+
 @app.route("/")
 def my_home():
+    print(url_for('static', filename='images/favicon.ico'))
     return render_template('index.html')
 
 
 @app.route("/about")
 def about():
     return render_template('about.html')
-
-
-@app.route("/blog")
-def blog():
-    return "<p>This is my blog</p>"
 
 
 @app.route("/blog/2020/dogs")
