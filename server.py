2db7dd70b2731088f903b101adb1dfd57f354f50
Andrei <andrei.baditoiu@gmail.com>
2024-09-16 11:45:06 +0300

    Added server.py, templates folder; modified script.py, style.css.

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route("/")
+def my_home():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/blog")
+def blog():
+    return "<p>This is my blog</p>"
+
+
+@app.route("/blog/2020/dogs")
+def blog2():
+    return "<p>This is my dog</p>"
