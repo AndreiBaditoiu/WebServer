d857a6af65c51e43afd85e26340d209ad44aa8e2
Andrei <andrei.baditoiu@gmail.com>
2024-09-22 22:49:02 +0300

    Modified: server.py, contact, thankyou, work (html).

--- server.py
+++ server.py
@@ -1,24 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect
 
 app = Flask(__name__)
 print(__name__)
 
 
-@app.route('/index.html')
+@app.route('/')
 def my_home():
     return render_template('index.html')
 
 
-@app.route("/works.html")
-def work():
-    return render_template('works.html')
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route("/about.html")
-def about():
-    return render_template('about.html')
-
-
-@app.route("/contact.html")
-def contact():
-    return render_template('contact.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong. Try again'
